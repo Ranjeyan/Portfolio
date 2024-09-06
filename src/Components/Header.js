4e02246c5356af1bade616ff7678c075eee49455
Ranjeyan A <amranjeyan03@gmail.com>
2024-09-06 09:56:27 +0530

    glass

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../Styles/Header.css'; 
 import Logo from '../assets/R.png';
+import Resume from '../assets/Ranjeyan_Resume.pdf'; 
 
 function Header() {
   return (
     <header className="header">
       <img src={Logo} alt="Logo" className="logo" />
 
-      <a href="mailto:ranjeyanari03@gmail.com" className="button">
-        <span className="button-text">Contact</span>
+      <a href={Resume} className="button" download="Resume.pdf">
+        <span className="button-text">Resume</span>
         <span className="arrow">↗</span>
       </a>
     </header>
