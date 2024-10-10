b734fe93ecdf082a919008ebd264eba273254069
Ranjeyan <amranjeyan03@gmail.com>
2024-10-10 12:13:39 +0530

    edited skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,14 +8,15 @@ function Skills() {
       <h2>Skills</h2>
       <ul className="skills-list">
         <li>Python</li>
-        <li>TensorFlow</li>
-        <li>PyTorch</li>
-        <li>Power BI</li>
         <li>Machine Learning</li>
+        <li>Power BI</li>
         <li>React.js</li>
         <li>Data Visualization</li>
         <li>SQL</li>
         <li>Flutter</li>
+        <li>Firebase</li>
+        <li>HTML</li>
+        <li>CSS</li>
       </ul>
     </div>
   );
