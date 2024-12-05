cf0d2a53d676590fe0f39e350079e0f33e6fe942
Ranjeyan <amranjeyan03@gmail.com>
2024-12-05 18:27:55 +0530

    add loader

--- src/App.js
+++ src/App.js
@@ -3,17 +3,47 @@ import Header from './Components/Header';
 import Hero from './Components/Hero';
 import Demo from './Components/Demo';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Ranjeyan from '../src/assets/R.png';
+
+// Custom Loader Component
+function Loader() {
+  return (
+    <div className="loader">
+      <img src={Ranjeyan} alt="Loading..." className="logo-loader" />
+      {/* You can add a spinner or animation here if you like */}
+    </div>
+  );
+}
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time (3 seconds or adjust based on actual needs)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <Router>
       <div className="App">
+        {/* Background element */}
         <div className='bg'></div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} /> 
-          <Route path="/demo" element={<Demo />} /> 
-        </Routes>
+
+        {/* Show custom loader while loading */}
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/demo" element={<Demo />} />
+            </Routes>
+          </>
+        )}
       </div>
     </Router>
   );
